b24a77c261f85cd3605826821b6f539a573ac2b7
Ankur Gupta <ankurg052@gmail.com>
2024-01-20 03:36:46 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/facefilterar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.facefilterar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/services.dart';
+import 'detector_view.dart';
+import 'face_detector_painter.dart';
+
+late ui.Image goggleImage;
+late ui.Image helmetImage;
+int filterNum = 0;
+final FaceDetector faceDetector = FaceDetector(
+  options: FaceDetectorOptions(
+    enableContours: true,
+    enableLandmarks: true,
+  ),
+);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  goggleImage = await loadImage('goggles.png');
+  helmetImage = await loadImage('helmet.png');
+  runApp(const MyApp());
+}
+
+Future<ui.Image> loadImage(String imageName) async {
+  final data = await rootBundle.load('assets/$imageName');
+  return decodeImageFromList(data.buffer.asUint8List());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const FaceDetectorView(),
+    );
+  }
+}
+
+class FaceDetectorView extends StatefulWidget {
+  const FaceDetectorView({super.key});
+
+  @override
+  State<FaceDetectorView> createState() => _FaceDetectorViewState();
+}
+
+class _FaceDetectorViewState extends State<FaceDetectorView> {
+  bool _canProcess = true;
+  bool _isBusy = false;
+  CustomPaint? _customPaint;
+  var _cameraLensDirection = CameraLensDirection.front;
+
+  @override
+  void dispose() {
+    _canProcess = false;
+    faceDetector.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DetectorView(
+      title: 'Face Detector',
+      customPaint: _customPaint,
+      onImage: _processImage,
+      initialCameraLensDirection: _cameraLensDirection,
+      onCameraLensDirectionChanged: (value) => _cameraLensDirection = value,
+    );
+  }
+
+  Future<void> _processImage(InputImage inputImage) async {
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    final faces = await faceDetector.processImage(inputImage);
+    if (inputImage.metadata?.size != null &&
+        inputImage.metadata?.rotation != null) {
+      final painter = FaceDetectorPainter(
+        faces,
+        inputImage.metadata!.size,
+        inputImage.metadata!.rotation,
+        _cameraLensDirection,
+      );
+      _customPaint = CustomPaint(foregroundPainter: painter);
+    }
+    _isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+}
